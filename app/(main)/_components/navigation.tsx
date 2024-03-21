cfea7434b4ac5a2489d8252c9e3d6b1b795e3c1d
R2 Desen <r2desen@gmail.com>
2024-03-20 21:36:07 -0300

    user items and logout opt

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
 import { ChevronLeft, MenuIcon } from "lucide-react";
@@ -6,6 +7,8 @@ import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 
 import { cn } from "@/lib/utils";
+
+import UserItem from "./user-item";
 
 export const Navigation = () => {
   const pathname = usePathname();
@@ -113,7 +116,7 @@ export const Navigation = () => {
           <ChevronLeft className="h-6 w-6" />
         </div>
         <div>
-          <p>Action items</p>
+          <UserItem />
         </div>
         <div className="mt-4">
           <p>Documents</p>
