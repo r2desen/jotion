f99923ac99b7c3c500e5f3e69e86bd2fde5fb921
R2 Desen <r2desen@gmail.com>
2024-03-21 22:15:46 -0300

    add archive feature

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -4,6 +4,7 @@
 import {
   ChevronLeft,
   MenuIcon,
+  Plus,
   PlusCircle,
   Search,
   Settings,
@@ -145,6 +146,7 @@ export const Navigation = () => {
         </div>
         <div className="mt-4">
           <DocumentList />
+          <Item onClick={handleCreate} icon={Plus} label="Add a page" />
         </div>
         <div
           onMouseDown={handleMouseDown}
