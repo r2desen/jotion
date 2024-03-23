bbad4e028a58a5ed3d9c534040535a95525b9452
R2 Desen <r2desen@gmail.com>
2024-03-22 22:35:12 -0300

    title real tiem editing

---
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -0,0 +1,9 @@
+const DocumentIdPage = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <span>DocumentId</span>
+    </div>
+  );
+};
+
+export default DocumentIdPage;
